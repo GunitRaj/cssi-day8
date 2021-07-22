e2627c9c0657f97701c09fc18f859c074aa1066f
Gunit Raj Waghrey <gunitrajwaghrey@gmail.com>
2021-07-22 17:46:43 +0000

    day 9 independent practice with spicy

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,34 +1,57 @@
-const getMessages = () => {
+// const getMessages = () => {
+//     const messagesRef = firebase.database().ref();
+//     messagesRef.on('value', (snapshot) => {
+//     const data = snapshot.val();
+//     findMessage(data);
+// });
+// }
+
+// const findMessage = (messages) => {
+//     const passcodeAttempt = document.querySelector('#passcode').value;
+//     for(let key in messages){
+//         const message = messages[key];
+//         if(passcodeAttempt === message.passcode){
+//             renderMessageAsHtml(message);
+//         }    
+//     }
+// };
+
+// const renderMessageAsHtml = (message) => {
+//     // Hide Input Form
+//     passcodeInput = document.querySelector('passcodeInput');
+//     passcodeInput.style.display = 'none';
+//     // Render messageas HTML
+//     const messageDiv = document.querySelector('#message');
+//     messageDiv.innerHTML = message;
+// }
+
+viewMsg = document.querySelector('#viewMsg');
+
+viewMsg.addEventListener('click', () => {
+    const userPasscodeGuess = document.querySelector('#passcode').value;
+    // findMessage(userPasscodeGuess);
+
     const messagesRef = firebase.database().ref();
-messagesRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
 
-    for(let key in data){
-        const message = data[key];
-        console.log(message)
-        if(myPass == message.passcode){
-            renderMessage(message);
+        for(let key in data){
+           if(data[key].passcode == userPasscodeGuess){
+               display(data[key])
+
+           }
         }
-        
-    }
+    })
 });
-}
 
-const findMessage = (myPass) => {
-const messagesRef = firebase.database().ref();
-messagesRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
+const display = (messageObject) => {
+    console.log(messageObject);
+    
+    // Render messageas HTML
+    const messageDiv = document.querySelector('#message');
+    messageDiv.innerHTML = messageObject.message
 
-    for(let key in data){
-        const message = data[key];
-        console.log(message)
-        
-    }
-});
-}
-document.querySelector("viewMsg").addEventListener('click', () => {
-    const passcode = document.querySelector('#passcode').value;
-    findMessage(passcode);
-})
+    // Hide Input Form
+    passcodeInput = document.querySelector('#passcodeInput');
+    passcodeInput.style.display = 'none';
+};
